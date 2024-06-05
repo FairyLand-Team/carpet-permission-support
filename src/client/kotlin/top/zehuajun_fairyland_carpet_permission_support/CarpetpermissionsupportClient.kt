d01ab75461c6b939feaad506deba1e1fe52041d7
zehuajun <zehuajun@zehuajun.top>
2024-06-05 18:37:04 +0800

    删除独立的客户端支持

--- src/client/kotlin/top/zehuajun_fairyland_carpet_permission_support/CarpetpermissionsupportClient.kt
+++
@@ -1,9 +0,0 @@
-package top.zehuajun_fairyland_carpet_permission_support
-
-import net.fabricmc.api.ClientModInitializer
-
-object CarpetpermissionsupportClient : ClientModInitializer {
-	override fun onInitializeClient() {
-		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
-	}
-}
